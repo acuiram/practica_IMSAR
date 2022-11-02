985c14d16d80971d5ef149e51ee171f11dff8ce7
acuiram <106117736+acuiram@users.noreply.github.com>
2022-11-02 15:48:49 +0200

    Add files via upload

---
+++ DC Motor/open_loop.m
@@ -0,0 +1,15 @@
+clc
+close all
+clear
+
+J = 0.01;
+b = 0.1;
+K = 0.01;
+R = 1;
+L = 0.5;
+s = tf('s');
+P_motor = K/((J*s+b)*(L*s+R)+K^2);
+
+linearSystemAnalyzer('step', P_motor, 0:0.1:5);
+
+rP_motor = 0.1/(0.5*s+1)
